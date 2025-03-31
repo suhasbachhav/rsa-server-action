0a6fe5a9fe26ba8dea546a41d4533480da844555
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-31 18:51:41 +0530

    error handling

--- app/show-data/page.js
+++ app/show-data/page.js
@@ -3,6 +3,7 @@ import UsePromiseDemo from "@/components/UsePromisesDemo";
 import Link from "next/link";
 import { Suspense } from "react";
 import fs from 'node:fs';
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 
 
@@ -11,15 +12,18 @@ export default async function showDataPage() {
     const data = fs.readFileSync('dummy-db.json', 'utf-8');
     const users = JSON.parse(data);
     resolve(users)
+    //reject(new Error('sdf'))
   }, 1000));
     
 
   return (
     <main>
       <Link href='../'>Home Page</Link>
-      <Suspense fallback={<p>Data is loading...</p>}>
-        <UsePromiseDemo fetchUsers={fetchUsersPromise} />
-      </Suspense>
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+        <Suspense fallback={<p>Data is loading...</p>}>
+          <UsePromiseDemo fetchUsers={fetchUsersPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
