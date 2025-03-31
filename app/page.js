b6945636d40f56828aa6202aa1d51466d99798f2
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-31 17:23:18 +0530

    fetch data

--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
-'use client';
 
 import ClientDemo from "@/components/ClientDemo";
+import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from "@/components/RSCDeemo";
 
 export default function Home() {
   return (
     <main>
-      <RSCDemo />
-      <ClientDemo />
+      <DataFetchingDemo />
     </main>
   );
 }
--- components/ClientDemo.js
+++ components/ClientDemo.js
@@ -1,3 +1,5 @@
+'use client';
+
 export default function ClientDemo({ children }) {
   console.log('ClientDemo rendered');
   return (
