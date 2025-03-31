ef286cd611466d23a087622c5a4f1492c496b911
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-31 18:25:16 +0530

    save actions

---
+++ app/add-data/page.js
@@ -0,0 +1,12 @@
+
+import ServerActionsDemo from "@/components/ServerActionsDemo";
+import Link from "next/link";
+
+export default function AddDataPage() {
+  return (
+    <main>
+      <Link href='../'>Home Page</Link>
+      <ServerActionsDemo />
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -2,11 +2,15 @@
 import ClientDemo from "@/components/ClientDemo";
 import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from "@/components/RSCDeemo";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <DataFetchingDemo />
+      <Link href='./add-data'>Add New Data</Link>
+      <ClientDemo />
+      <RSCDemo />
     </main>
   );
 }
